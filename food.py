0730505e87a768cd06b5846fedd82d87a16fbf51
Leo Vidarte <lvidarte@gmail.com>
2024-06-09 22:49:58 -0300

    Initial commit

---
+++ food.py
@@ -0,0 +1,33 @@
+import random
+from typing import Tuple
+
+from board import Board
+from snake import Snake
+
+
+class Food:
+
+    COLOR: str = 'red'
+
+    def __init__(self, board: Board) -> None:
+        self.board: Board = board
+        self.position: Tuple[int, int] = (0, 0)
+
+    def draw(self) -> None:
+        x, y = self.position
+        self.board.canvas.create_rectangle(
+            x,
+            y,
+            x + self.board.cell_size,
+            y + self.board.cell_size,
+            fill=self.COLOR,
+            outline=self.board.COLOR
+        )
+
+    def create_food(self, snake: Snake) -> None:
+        available_positions = [
+            (x, y) for x in range(0, self.board.width, self.board.cell_size)
+            for y in range(0, self.board.height, self.board.cell_size)
+            if (x, y) not in snake.segments
+        ]
+        self.position = random.choice(available_positions)
